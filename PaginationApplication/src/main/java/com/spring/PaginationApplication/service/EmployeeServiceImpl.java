7678e32553b7696e578ff2c7bbe3a0ab04f27b4b
rajch <rajch@192.168.0.4>
2020-03-05 20:13:31 +0530

    created service class implementation.

--- PaginationApplication/src/main/java/com/spring/PaginationApplication/service/EmployeeServiceImpl.java
+++ PaginationApplication/src/main/java/com/spring/PaginationApplication/service/EmployeeServiceImpl.java
@@ -1,18 +1,31 @@
 package com.spring.PaginationApplication.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.spring.PaginationApplication.entity.EmployeeEntity;
 import com.spring.PaginationApplication.repo.EmployeeRepository;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+	@Autowired
 	EmployeeRepository employeeRepository;
 	@Override
 	public List<EmployeeEntity> getAllEmployees(Integer pageNo, Integer pageSize, String sortBy) {
-		// TODO Auto-generated method stub
-		return null;
+		Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+		Page<EmployeeEntity> pagedResult = employeeRepository.findAll(paging);
+        
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<EmployeeEntity>();
+        }
 	}
 
 }
